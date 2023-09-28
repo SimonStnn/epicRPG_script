1e4521f496907dd60a8656965fba198602f3e44b
SimonStnn <simon.stijnen.23@gmail.com>
2023-09-28 11:50:07 +0200

    Rewrite the way commands are handled

--- src/main.py
+++ src/main.py
@@ -1,37 +1,90 @@
-import pyautogui
 import time
+import pyautogui
 
-MOVE_DURATION = .3
-TYPE_INTERVAL = .1
+MOVE_DURATION = 0.3
+TYPE_INTERVAL = 0.1
 
-# Every x hunts do a /heal
-HEAL_INTERVAL = 10
 
-def open_discord():
-    pyautogui.moveTo(1900,2100, duration=MOVE_DURATION)
-    pyautogui.click()
-    pyautogui.moveTo(1900,1900, duration=MOVE_DURATION)
-    pyautogui.click()
-    
-def run_command(command: str, *args: str):
-    pyautogui.typewrite(command, interval=TYPE_INTERVAL)
-    time.sleep(.5)
-    
-    for arg in args:
-        pyautogui.typewrite("\t\t\t", interval=TYPE_INTERVAL*3)
-        time.sleep(.5)
-        pyautogui.typewrite(arg, interval=TYPE_INTERVAL)
-        time.sleep(.5)
-        
-    pyautogui.typewrite("\t\n\n", interval=TYPE_INTERVAL*3)
-    time.sleep(.5)
+class Command:
+    def __init__(self, cmd: str, delay: int, heal_interval: int | None = None) -> None:
+        self.cmd = cmd
+        self.delay = delay
+        self.heal_interval = heal_interval
 
-open_discord()
-while True:
-    run_command("/farm")
-    for i in range(HEAL_INTERVAL):
-        run_command(i)
-        run_command("/hunt")
-        time.sleep(60)
-    run_command("/heal")
-    run_command("/mine")
+        self.last_executed = 0
+        self.now = 0
+
+        self.counter = 0
+
+    @property
+    def should_execute(self) -> bool:
+        self.now = time.time()
+        if self.now - self.last_executed > self.delay:
+            self.last_executed = self.now
+            return True
+        return False
+
+    def execute(self):
+        self.counter += 1
+
+        if self.heal_interval:
+            if self.counter % self.heal_interval == 0:
+                self._run_command("/heal")
+            else:
+                print(
+                    "\tHealing in "
+                    + str(self.heal_interval - (self.counter % self.heal_interval))
+                    + " commands"
+                )
+
+        self._run_command(self.cmd)
+
+    def _run_command(self, command: str):
+        print("\tRunning command: " + command)
+        # return
+        pyautogui.typewrite(command, interval=TYPE_INTERVAL)
+        pyautogui.typewrite("\t\n\n", interval=TYPE_INTERVAL)
+        # pyautogui.press("enter")
+        time.sleep(0.1)
+
+
+class Script:
+    commands: list[Command] = []
+
+    def __init__(self) -> None:
+        pass
+
+    def run(self):
+        self.click(1900, 2100)
+        time.sleep(5)
+        while True:
+            print("loop")
+            for command in self.commands:
+                if command.should_execute:
+                    command.execute()
+            time.sleep(0.5)
+
+    def click(self, x: int, y: int):
+        pyautogui.moveTo(x, y, duration=MOVE_DURATION)
+        time.sleep(0.1)
+        pyautogui.click()
+        time.sleep(0.1)
+
+    def register(self, command: str, delay: int, *, heal_inteval: int = None):
+        self.commands.append(Command(command, delay, heal_inteval))
+
+
+if __name__ == "__main__":
+    script = Script()
+    # Run /hunt every 60 seconds, and every 10 times do a /heal
+    script.register("/hunt", 1 * 60, heal_inteval=10)
+    # Run /adventure every 60 minutes
+    script.register("/adventure", 60 * 60)
+    # RUn /farm every 10 minutes
+    script.register("/farm", 10 * 60)
+
+    print("Script started, press CTRL+C to stop")
+    try:
+        script.run()
+    except KeyboardInterrupt:
+        print("Script stopped, Bye!")
